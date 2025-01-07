78708de10af609e4b1e31374216f6170e586cf66
amit redhu <redhus341@gmail.com>
2025-01-08 01:29:04 +0530

    add prmote model

--- testing/prmote_model.py
+++ testing/prmote_model.py
@@ -3,11 +3,11 @@ import mlflow
 
 def promote_model():
     # Set up AWS MLflow tracking URI
-    mlflow.set_tracking_uri("http://ec2-54-196-109-131.compute-1.amazonaws.com:5000/")
+    mlflow.set_tracking_uri("http://13.238.159.116:5000/")
 
     client = mlflow.MlflowClient()
 
-    model_name = "yt_chrome_plugin_model"
+    model_name = "plugin_model"
     # Get the latest version in staging
     latest_version_staging = client.get_latest_versions(model_name, stages=["Staging"])[0].version
 
